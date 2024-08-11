80ef8cefcea68371a3f40d3e3b1a88cf0b0e07d8
Tharo Chan <tharochanrith@gmail.com>
2024-08-11 17:20:19 +0700

    Add delete feature

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,17 +1,14 @@
-import FullPageImageView from "~/app/common/full-page-image-view";
+import { FullPageImageView } from "~/app/common/full-page-image-view";
 import { Modal } from "./modal";
 
-export default function PhotoModal({
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  const idAsNumber = parseInt(photoId);
-  if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
-
   return (
     <Modal>
-      <FullPageImageView photoId={idAsNumber} />
+      <FullPageImageView photoId={photoId} />
     </Modal>
   );
 }
--- src/app/common/full-page-image-view.tsx
+++ src/app/common/full-page-image-view.tsx
@@ -1,9 +1,14 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { Button } from "~/components/ui/button";
+import { deleteImage, getImage } from "~/server/queries";
 
-export default async function FullPageImageView(props: { photoId: number }) {
-  const image = await getImage(props.photoId);
-  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+export async function FullPageImageView(props: { photoId: string }) {
+  const idAsNumber = Number(props.photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
+
+  const userInfo = await clerkClient.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
@@ -15,12 +20,26 @@ export default async function FullPageImageView(props: { photoId: number }) {
 
         <div className="p-2">
           <div>Uploaded By:</div>
-          <div>{uploaderInfo.fullName}</div>
+          <div>{userInfo.fullName}</div>
         </div>
 
         <div className="p-2">
           <div>Created On:</div>
           <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
+
+        <div className="p-2">
+          <form
+            action={async () => {
+              "use server";
+
+              await deleteImage(idAsNumber);
+            }}
+          >
+            <Button type="submit" variant="destructive">
+              Delete
+            </Button>
+          </form>
         </div>
       </div>
     </div>
