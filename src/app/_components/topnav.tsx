d18174e68946c05c5d6e331bc3e598eba58f4d60
Tharo Chan <tharochanrith@gmail.com>
2024-08-11 12:35:34 +0700

    Uploadthing added

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <div>
+      <div className="flex flex-row">
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
         <SignedOut>
